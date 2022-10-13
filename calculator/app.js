24367b156213213ca38582571c1445b123b41463
ReugeneF <reugene_business@hotmail.com>
2022-10-13 11:08:15 +0200

    oke

--- calculator/app.js
+++ calculator/app.js
@@ -2,32 +2,49 @@ let n1 = document.getElementById('num_1');
 let n2 = document.getElementById('num_2');
 let answer = document.getElementById('answer');
 
+//Calculates number when using multiply
 function multiply(){
-    let num1 = n1.value;
-    let num2 = n2.value;
-    let answer = num1 * num2;
-}
+    let answer = n1.value* n2.value;
 
+    if(n1.value < 0 && n2.value < 0){
+        document.getElementById('answer').innerText = "inavlid number"
+    } else{
+        document.getElementById('answer').innerText = "Het antwoord is " + answer;
+    }
+}
+//Calculates number when dividing
 function devide(){
-    let num1 = n1.value;
-    let num2 = n2.value;
-    let answer = num1 / num2;
+    let answer = n1.value / n2.value;
+    document.getElementById('answer').innerText = answer;
+    
+    if(n1.value < 0 && n2.value < 0){
+        document.getElementById('answer').innerText = "inavlid number"
+    } else{
+        document.getElementById('answer').innerText = "Het antwoord is " + answer;
+    }
 }
-
+//Calculates number when using plus
 function plus(){
-    let num1 = n1.value;
-    let num2 = n2.value;
-    let answer = Number(num1) + Number(num2);
+    let answer = Number(n1.value) + Number(n2.value);
+    document.getElementById('answer').innerText = answer;
+    
+    if(n1.value < 0 && n2.value < 0){
+        document.getElementById('answer').innerText = "inavlid number"
+    } else{
+        document.getElementById('answer').innerText = "Het antwoord is " + answer;
+    }
 }
+//Calculates number when using minus
 function minus(){
-    let num1 = n1.value;
-    let num2 = n2.value;
-    let answer = num1 - num2;
-}
-function anw(){
-document.getElementById('answer').innerText = answer;
-}
-anw()
+    let answer = n1.value - n2.value;
+    document.getElementById('answer').innerText = answer;
+
+    if(n1.value < 0 && n2.value < 0){
+        document.getElementById('answer').innerText = "inavlid number"
+    } else{
+        document.getElementById('answer').innerText = "Het antwoord is " + answer;
+    }
+} 
 
 
 
